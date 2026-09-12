0cf2b0681ab64012bafe44323376c8baf724122e
linastens-sys <linastensland@gmail.com>
2026-09-12 14:33:31 -0400

    Update NeoPixelLightShow.py

--- NeoPixelLightShow.py
+++ NeoPixelLightShow.py
@@ -1,8 +1,8 @@
 import neopixel #importing the library
 from machine import Pin # another way of importing a library
 lights = neopixel.NeoPixel(Pin(15),2) # 0 is the Pin for neopixel and 4 is the number of lights
-#[0] = (0,20,20)
-#lights[1] = (0,20,20)# set the color of 0th light to purple 
+lights[0] = (0,20,20)
+lights[1] = (0,20,20)# set the color of 0th light to purple 
 lights.write()
 
 import time
